e0d1dc0299722404f1357b70e9a714a62a5035b9
cavidan <cavidaneken1@gmail.com>
2022-08-16 20:46:53 +0300

    3.GCAVIB1

--- src/Day3GitGithub.java
+++ src/Day3GitGithub.java
@@ -2,7 +2,7 @@ public class Day3GitGithub {
     public static void main(String[] args) {
         System.out.println("3. gün 1. deneme");
         System.out.println("3. gün 2. deneme");
-
+        System.out.println("3. gün cavi branch d1");
 
     }
 }
