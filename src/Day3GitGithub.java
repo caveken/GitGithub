d04246e1b552856e754ad81723a11ceee377de49
cavidan <cavidaneken1@gmail.com>
2022-08-16 21:15:44 +0300

    3.GM4

--- src/Day3GitGithub.java
+++ src/Day3GitGithub.java
@@ -3,6 +3,8 @@ public class Day3GitGithub {
         System.out.println("3. gün 1. deneme");
         System.out.println("3. gün 2. deneme");
         System.out.println("3. gün cavi branch d1");
+        System.out.println("3. gün master 3. deneme");
+        System.out.println("3. gün master 4. deneme");
 
     }
 }
