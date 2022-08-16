84839634d8f82ae1226559b5fdf617a2846afbc0
caveken <104344162+caveken@users.noreply.github.com>
2022-08-16 22:12:55 +0300

    Update Day1Git.java

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -3,6 +3,7 @@ public class Day1Git {
         System.out.println("hello git 1. deneme");
         System.out.println("2. deneme");
         System.out.println("3. deneme");
+        System.out.println("4. deneme");
 
 
     }
